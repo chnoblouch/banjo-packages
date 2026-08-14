afe7b344f810d4b9169613ac1d7f81f74013be6d
Marino von Wattenwyl <m.v.wattenwyl@gmail.com>
2026-08-14 22:51:07 +0200

    curl: include brotli

--- scripts/build_curl.py
+++ scripts/build_curl.py
@@ -5,6 +5,7 @@ GIT_TAG = "curl-8_21_0"
 OPENSSL_GIT_TAG = "openssl-3.6.3"
 ZLIB_GIT_TAG = "v1.3.2"
 ZSTD_GIT_TAG = "v1.5.7"
+BROTLI_GIT_TAG = "v1.2.0"
 LIBSSH2_GIT_TAG = "libssh2-1.11.1"
 
 SETOPT_PATCH = """
@@ -33,6 +34,7 @@ if __name__ == "__main__":
     building.git_clone("openssl", "https://github.com/openssl/openssl.git", OPENSSL_GIT_TAG)
     building.git_clone("zlib", "https://github.com/madler/zlib.git", ZLIB_GIT_TAG)
     building.git_clone("zstd", "https://github.com/facebook/zstd.git", ZSTD_GIT_TAG)
+    building.git_clone("brotli", "https://github.com/google/brotli.git", BROTLI_GIT_TAG)
     building.git_clone("libssh2", "https://github.com/libssh2/libssh2.git", LIBSSH2_GIT_TAG)
 
     with open(building.get_path("curl/lib/setopt.c"), "r+") as f:
@@ -49,6 +51,8 @@ if __name__ == "__main__":
     crypto_library_name = "libcrypto.lib" if building.is_windows() else "libcrypto.a"
     zlib_library_name = "zs.lib" if building.is_windows() else "libz.a"
     zstd_library_name = "zstd_static.lib" if building.is_windows() else "libzstd.a"
+    brotlicommon_library_name = "brotlicommon-static.lib" if building.is_windows() else "libbrotlicommon-static.a"
+    brotlidec_library_name = "brotlidec-static.lib" if building.is_windows() else "libbrotlidec-static.a"
     libssh2_library_name = "libssh2.lib" if building.is_windows() else "libssh2.a"
 
     openssl_path = building.get_path("openssl")
@@ -65,10 +69,11 @@ if __name__ == "__main__":
     subprocess.run(command, cwd=openssl_path)
 
     openssl_make_tool = "nmake" if building.is_windows() else "make"
-    subprocess.run([openssl_make_tool, "install"], cwd=openssl_path)
+    subprocess.run([openssl_make_tool, "install_sw"], cwd=openssl_path)
 
     zlib_install_path = building.cmake_build("zlib", [])
     zstd_install_path = building.cmake_build("zstd/build/cmake", [])
+    brotli_install_path = building.cmake_build("brotli", ["-DBROTLI_BUILD_FOR_PACKAGE=ON"])
 
     configure_args = [
         "-DCRYPTO_BACKEND=OpenSSL",
@@ -88,12 +93,15 @@ if __name__ == "__main__":
         f"-DZLIB_LIBRARY={zlib_install_path}/lib/{zlib_library_name}",
         f"-DZSTD_INCLUDE_DIR={zstd_install_path}/include",
         f"-DZSTD_LIBRARY={zstd_install_path}/lib/{zstd_library_name}",
+        f"-DBROTLI_INCLUDE_DIR={brotli_install_path}/include",
+        f"-DBROTLICOMMON_LIBRARY={brotli_install_path}/lib/{brotlicommon_library_name}",
+        f"-DBROTLIDEC_LIBRARY={brotli_install_path}/lib/{brotlidec_library_name}",
         f"-DLIBSSH2_LIBRARY={libssh2_install_path}/lib/{libssh2_library_name}",
         f"-DLIBSSH2_INCLUDE_DIR={libssh2_install_path}/include",
+
         "-DUSE_LIBIDN2=OFF",
         "-DUSE_NGHTTP2=OFF",
         "-DCURL_USE_LIBPSL=OFF",
-        "-DCURL_BROTLI=OFF",
         "-DCURL_DISABLE_LDAP=ON",
     ]
 
@@ -104,6 +112,8 @@ if __name__ == "__main__":
     building.copy_libraries(openssl_install_path / "lib", [ssl_library_name, crypto_library_name])
     building.copy_libraries(zlib_install_path / "lib", [zlib_library_name])
     building.copy_libraries(zstd_install_path / "lib", [zstd_library_name])
+    building.copy_libraries(brotli_install_path / "lib", [brotlicommon_library_name])
+    building.copy_libraries(brotli_install_path / "lib", [brotlidec_library_name])
     building.copy_libraries(libssh2_install_path / "lib", [libssh2_library_name])
 
     building.generate_bindings(install_path / "include", "curl")
@@ -112,4 +122,5 @@ if __name__ == "__main__":
     building.copy_license(building.get_path("openssl/LICENSE.txt"), "openssl")
     building.copy_license(building.get_path("zlib/LICENSE"), "zlib")
     building.copy_license(building.get_path("zstd/LICENSE"), "zstd")
+    building.copy_license(building.get_path("brotli/LICENSE"), "brotli")
     building.copy_license(building.get_path("libssh2/COPYING"), "libssh2")
